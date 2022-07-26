73fdd18402ac86e3aca6a5bf461f5fa979c542bc
sigma2647 <1169446204@qq.com>
2022-07-26 15:20:11 +0800

    delete a line

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -136,13 +136,10 @@
 
 (use-package magit
   :custom
-  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1)
-)
+  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
 
 (use-package evil-magit
-  :after magit
-)
-
+  :after magit)
 
 
 
