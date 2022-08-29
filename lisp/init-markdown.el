02c04cc675af9b49ae4095739e0b6b9389be5f98
sigma2647 <1169446204@qq.com>
2022-08-29 17:51:51 +0800

    upda

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -2,4 +2,16 @@
             '(("^ *\\([-]\\) "
             (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
 
+; (evil-define-key 'motion evil-markdown-mode-map
+;        ;; (kbd (concat "g" .left)) 'markdown-up-heading
+;        ;; (kbd (concat "g" .right)) (lambda () (interactive) (markdown-next-heading))
+;        ;; (kbd (concat "g" .up)) 'markdown-backward-same-level
+;        ;; (kbd (concat "g" .down)) 'markdown-forward-same-level)
+;        (kbd (concat "g" .left)) 'markdown-up-heading
+;        (kbd (concat "g" .right)) (lambda () (interactive) (markdown-next-heading))
+;        (kbd (concat "g" .up)) 'markdown-backward-same-level
+;        (kbd (concat "g" .down)) 'markdown-forward-same-level)
+
+
+
 (provide 'init-markdown)
