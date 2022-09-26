16bf6c2fdb65002dd0692c1dde6eeb32ef216a31
sigma2647_macmini <1169446204@qq.com>
2022-09-27 03:00:25 +0800

    upda

--- lisp/init-startup.el
+++ lisp/init-startup.el
@@ -5,7 +5,7 @@
 
 ; 设置logo
 (defcustom centaur-logo (expand-file-name
-                         (if (display-graphic-p) "~/.emacs.d/dashboard/logo.png" "~/.emacs.d/dashboard/banner.txt")
+                         (if (display-graphic-p) "~/.emacs.d/dashboard/eva.png" "~/.emacs.d/dashboard/banner.txt")
                          user-emacs-directory)
   "Set Centaur logo. nil means official logo."
   :group 'centaur
